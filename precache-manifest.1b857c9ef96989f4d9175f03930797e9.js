fe43a85cfbd03ea0ce3807500a62add3e6d779e7
Pavlo Bessmertnyy <pavlo.bessmertnyy@globallogic.com>
2020-09-20 20:36:58 +0300

    Updates

---
+++ precache-manifest.1b857c9ef96989f4d9175f03930797e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77408e803d7bec35a694051ec6797bb0",
+    "url": "/react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "ee5af1b725389c2fdda4",
+    "url": "/react-hw-04-movies/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f0b0f97bbdeb0e1528b",
+    "url": "/react-hw-04-movies/static/js/2.ac27ad32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hw-04-movies/static/js/2.ac27ad32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5af1b725389c2fdda4",
+    "url": "/react-hw-04-movies/static/js/main.e57d55a9.chunk.js"
+  },
+  {
+    "revision": "e2395479082b559ca45d",
+    "url": "/react-hw-04-movies/static/js/runtime-main.16c3efb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-hw-04-movies/static/media/logo.5d5d9eef.svg"
+  }
+]);
